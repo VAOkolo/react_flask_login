19c03f79882305888c01616844037e1564f03c30
Joshua Cerdenia <joshcerdenia@gmail.com>
2022-07-29 23:58:24 +0800

    Implement user login via API

--- client/src/views/Login.js
+++ client/src/views/Login.js
@@ -1,20 +1,66 @@
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useUserContext } from "../context/UserContext";
 import Layout from "../components/Layout";
 import TextInput from "../components/TextInput";
 import Button from "../components/Button";
-import { Link } from "react-router-dom";
+import Spinner from "../components/Spinner";
+import * as api from "../api";
 
 const Login = () => {
+  const [pending, setPending] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const [username, setUsername] = useState(null);
+  const [password, setPassword] = useState(null);
+
+  const { user, setUser } = useUserContext();
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setPending(true);
+    const res = await api.login({ username, password });
+    setErrorMessage(res.error || null);
+    setPending(false);
+    setUser(res.token);
+  };
+
+  useEffect(() => {
+    if (user) {
+      navigate("/home");
+    }
+  }, [user, navigate]);
+
   return (
     <Layout>
       <h1>Welcome!</h1>
       <p>Log in with your username and password.</p>
 
-      <form>
-        <TextInput id="floatingText" type="text" label="Username" />
-        <TextInput id="floatingPassword" type="password" label="Password" />
+      <form onSubmit={handleSubmit}>
+        <TextInput
+          id="floatingText"
+          type="text"
+          label="Username"
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <TextInput
+          id="floatingPassword"
+          type="password"
+          label="Password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
 
-        <Button type="submit" className="mt-3">
-          Log In
+        {errorMessage ? (
+          <div className="text-red-700">{errorMessage}</div>
+        ) : null}
+
+        <Button
+          disabled={!username || !password || pending}
+          type="submit"
+          className="mt-3"
+        >
+          {pending ? <Spinner className="w-4 h-4" /> : null} Log In
         </Button>
       </form>
 
