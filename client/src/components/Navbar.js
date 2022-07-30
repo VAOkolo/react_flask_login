9cb8f1876833e339633943447e74415a99f64584
Joshua Cerdenia <joshcerdenia@gmail.com>
2022-07-30 16:42:58 +0800

    Change navnar sign up and logout links depending on user context

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,7 +1,24 @@
+import { useUserContext } from "../context/UserContext";
+
 const Navbar = () => {
+  const { user } = useUserContext();
+
   return (
     <div>
-      <nav className="relative w-full flex flex-wrap items-center justify-between py-4 bg-gray-100 text-gray-500 hover:text-gray-700 focus:text-gray-700 shadow-lg">
+      <nav
+        className="relative
+        w-full 
+        flex 
+        flex-wrap 
+        items-center 
+        justify-between 
+        py-4 
+        bg-gray-100 
+        text-gray-500 
+        hover:text-gray-700 
+        focus:text-gray-700 
+        shadow-lg"
+      >
         <div className="container-fluid flex flex-wrap items-center px-6">
           <a className="text-xl text-black font-semibold" href="/">
             Demo
@@ -9,9 +26,18 @@ const Navbar = () => {
         </div>
 
         <div className="navbar-nav px-6">
-          <a className="nav-link active" href="/register">
-            Sign Up
-          </a>
+          {!user.username ? (
+            <a className="nav-link active" href="/register">
+              Sign Up
+            </a>
+          ) : (
+            <div>
+              <span className="font-bold mr-6">{user.username}</span>
+              <a className="nav-link active" href="/logout">
+                Log Out
+              </a>
+            </div>
+          )}
         </div>
       </nav>
     </div>
