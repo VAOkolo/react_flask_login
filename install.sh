0b387bf5a17660dca9b7422a34684f680a06dd9c
Joshua Cerdenia <joshcerdenia@gmail.com>
2022-07-30 23:35:36 +0800

    Update scripts

--- install.sh
+++ install.sh
@@ -3,4 +3,6 @@ cd client
 npm i
 cd ..
 cd server
+python3 -m venv venv
+source venv/bin/activate
 pip install -r requirements.txt
--- start.sh
+++ start.sh
@@ -3,5 +3,6 @@ npm --prefix client run start &
 cd server
 source venv/bin/activate
 python -c 'from app import db; db.create_all()'
+export FLASK_APP=app.py
 export FLASK_ENV=development
 flask run
